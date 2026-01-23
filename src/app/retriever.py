c935ec4120272ac90b046f0de8d722dfe33cc527
sathwik-reddy-m <sathwikreddym79@gmail.com>
2026-01-23 13:44:44 +0530

    feat: add retrieval logic using FAISS similarity search

---
+++ src/app/retriever.py
@@ -0,0 +1,24 @@
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def retrieve_documents(query: str, k: int = 3):
+    """
+    Retrieve top-k relevant documents for a query.
+    """
+
+    # 1. Load embeddings model (must be SAME as ingestion)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    # 2. Load FAISS index from disk
+    vectorstore = FAISS.load_local(
+        "faiss_index",
+        embeddings,
+        allow_dangerous_deserialization=True,
+    )
+
+    # 3. Perform similarity search
+    documents = vectorstore.similarity_search(query, k=k)
+
+    return documents
