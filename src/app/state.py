7058e771030190a7f168f4b62a56dc9c60763ca5
sathwik-reddy-m <sathwikreddym79@gmail.com>
2026-01-25 18:41:29 +0530

    feat: add LangGraph state definition

---
+++ src/app/state.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class GraphState(BaseModel):
+    """
+    Shared state that flows through the LangGraph
+    """
+
+    query: str
+    retrieved_docs: Optional[list[str]] = None
+    answer: Optional[str] = None
+
+    
